b48d979ea391122efcbd560b719acc9c278bf8d6
masterbogdan <maiskiychavk@gmail.com>
2020-05-28 23:52:36 +0300

    Rewrite some code, add basic show city

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
 'use strict';
 
-    let cityId = 361058;
+    let cityId = '361058';
     let cityIdArr = function() {
     const btns = document.querySelectorAll('.btn');
         for (let i = 0; i < btns.length; i++) {
             btns[i].addEventListener('click', function(){
                 cityId = btns[i].value;
-                console.log();
                 getWeather(cityId);
             });
         }
          console.log(btns);
     };
-    cityId = cityIdArr;
+
     console.log(cityId);
-    console.log(cityIdArr());
+    console.log(cityIdArr(361058));
 
-    
-
+    getWeather(cityId);
 
 function getWeather(cityId) {
-    const url = encodeURI(`https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=ab301f47bc888b6dd8fbe9ec9461c9e2`);   
+    const url = encodeURI(`https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=ab301f47bc888b6dd8fbe9ec9461c9e2&lang=ru`);   
 fetch(url)
         .then(function (resp) { return resp.json() })
         .then(function (data) {
             console.log(data);
             document.querySelector('.city-name').textContent = data.name;
-            document.querySelector('.degrees').innerHTML= Math.round(data.main.temp - 273) + '&deg;';
+            document.querySelector('.temperature').innerHTML= 'Температура ' + Math.round(data.main.temp - 273) + '&deg;';
+            document.querySelector('.min-temperature').innerHTML= 'Минимальная температура ' + Math.round(data.main.temp_min - 273) + '&deg;';
+            document.querySelector('.max-temperature').innerHTML= 'Максимальная температура ' + Math.round(data.main.temp_max - 273) + '&deg;';
             document.querySelector('.condition').textContent = data.weather[0].description;
+            document.querySelector('.icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">`;
+            document.querySelector('.pressure').textContent = `Давление ${data.main.pressure} мбар`;
+            document.querySelector('.wind').innerHTML = `Скорость ветра ${data.wind.speed} м/с`;
+            document.querySelector('.sunset').textContent = data.sys.sunset;
         })
         .catch(function () {
 
